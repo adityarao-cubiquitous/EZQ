299c3fd4a54c03364312752b272dedb291dc0199
Lasyapriya <lasyabharadwaj541@gmail.com>
2026-08-11 13:18:05 +0530

    feat(queue): add special notes and phone editing UX

--- lib/core/widgets/ezq_text_field.dart
+++ lib/core/widgets/ezq_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../constants/app_colors.dart';
 
@@ -11,6 +12,10 @@ class EzqTextField extends StatelessWidget {
     this.keyboardType,
     this.maxLines = 1,
     this.prefixText,
+    this.prefix,
+    this.focusNode,
+    this.inputFormatters,
+    this.hintStyle,
     this.validator,
   });
 
@@ -20,6 +25,10 @@ class EzqTextField extends StatelessWidget {
   final TextInputType? keyboardType;
   final int maxLines;
   final String? prefixText;
+  final Widget? prefix;
+  final FocusNode? focusNode;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextStyle? hintStyle;
   final String? Function(String?)? validator;
 
   @override
@@ -41,14 +50,25 @@ class EzqTextField extends StatelessWidget {
         ),
         TextFormField(
           controller: controller,
+          focusNode: focusNode,
           keyboardType: keyboardType,
+          inputFormatters: inputFormatters,
           maxLines: maxLines,
           validator: validator,
           style: const TextStyle(color: AppColors.navyText, fontSize: 16),
           decoration: InputDecoration(
             hintText: hintText,
             prefixText: prefixText,
-            hintStyle: const TextStyle(color: Color(0xFF6B7280)),
+            prefixIcon: prefix == null
+                ? null
+                : Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 2),
+                    child: Center(widthFactor: 1, child: prefix),
+                  ),
+            prefixIconConstraints: prefix == null
+                ? null
+                : const BoxConstraints(minWidth: 0, minHeight: 0),
+            hintStyle: hintStyle ?? const TextStyle(color: Color(0xFF6B7280)),
           ),
         ),
       ],
